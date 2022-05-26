feb070cc83b30b970b48a8ddab5539e7b8d69b15
wingmatt <matt@wingwebhelp.com>
2022-05-25 22:27:01 -0700

    Remove Icon Component. Will use react-icon instead

--- components/Accordion/Icon.tsx
+++
@@ -1,9 +0,0 @@
-// TODO - add mechanism for making icons partially transparent
-
-const Icon = (props: {url: string}) => {
-  return (
-    <img className="icon" src={props.url} />
-  )
-}
-
-export default Icon
