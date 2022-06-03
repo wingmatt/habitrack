645af0e19aa03292ed59601428df68e6f5a04f92
wingmatt <matt@wingwebhelp.com>
2022-06-02 19:12:21 -0700

    Add first component unit test

---
+++ __tests__/task.test.js
@@ -0,0 +1,15 @@
+import "@testing-library/jest-dom";
+import React from 'react'
+import { render,screen } from "@testing-library/react"
+import Task from "../components/Accordion/Task"
+
+test('Task name renders in the component', () => {
+  //arrange
+  render(<Task name="Test name please ignore" complete="false" />)
+
+  //act
+  const taskName = screen.getByText("Test name please ignore");
+
+  //assert
+  expect(taskName).toBeInTheDocument();
+})
