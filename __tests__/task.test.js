41c6b7892dd5955e82d82cb1ed6a4167e64357cd
wingmatt <matt@wingwebhelp.com>
2022-06-07 08:05:05 -0700

    Remove React import. Use boolean instead of string

--- __tests__/task.test.js
+++ __tests__/task.test.js
@@ -1,11 +1,10 @@
 import "@testing-library/jest-dom";
-import React from 'react'
 import { render,screen } from "@testing-library/react"
 import Task from "../components/Accordion/Task"
 
 test('Task name renders in the component', () => {
   //arrange
-  render(<Task name="Test name please ignore" complete="false" />)
+  render(<Task name="Test name please ignore" complete={false} />)
 
   //act
   const taskName = screen.getByText("Test name please ignore");
