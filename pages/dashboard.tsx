e68c9e8804435dd6fa0ec474900587c70a3e630e
wingmatt <matt@wingwebhelp.com>
2022-06-15 07:35:15 -0700

    Use Layout in /dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,9 +1,11 @@
 import AuthRequired from '../helpers/AuthRequired'
 import Accordion from '../components/Accordion/'
+import Layout from '../components/Layout'
 
 export default function Dashboard() {
 
   return (
+    <Layout>
     <AuthRequired>
       <div className="container" style={{ padding: '50px 0 100px 0' }}>
         <Accordion key="1" title="Morning" iconUrl='something' tasks={[{complete: false, name: "mornin'"}, {complete: true, name: "Second morning thing"}]}></Accordion>
@@ -11,6 +13,7 @@ export default function Dashboard() {
         <Accordion key="3" title="Evening" iconUrl='something' tasks={[{complete: false, name: "evenin'"}]}></Accordion>
       </div>
     </AuthRequired>
+    </Layout>
   )
 
   
