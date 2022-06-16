8fc438eb23110fdbf7d3b05ab6e1350c63786085
wingmatt <matt@wingwebhelp.com>
2022-06-16 07:17:02 -0700

    Init /habits page w/ description of future content

---
+++ pages/habits.tsx
@@ -0,0 +1,33 @@
+import Layout from "../components/Layout";
+import AuthRequired from "../helpers/AuthRequired";
+import Habit from "../components/Habit"
+
+export default function Habits() {
+  return (
+    <Layout>
+      <AuthRequired>
+        <h2>Manage Your Habits!</h2>
+        <p>
+          You can edit all the different data about your habits here, such as:
+        </p>
+        <ul>
+          <li>Recurrance frequency: How often do you want to do the thing?</li>
+          <li>Sharing settings: Who else can do the thing in your stead?</li>
+          <li>Name: What do you call the habit?</li>
+          <li>
+            Description: Any helpful info that someone doing this should have on
+            hand?
+          </li>
+          <li>Time of day: When do you want this habit to be done?</li>
+        </ul>
+        <h2>My Habits</h2>
+        <ul>
+          <li>
+            <Habit/>
+          </li>
+        </ul>
+        <h2>Habits Shared with Me</h2>
+      </AuthRequired>
+    </Layout>
+  );
+}
