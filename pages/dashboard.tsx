7f8ed6f53731a80b39c10ca202d3f8b15de9594a
wingmatt <matt@wingwebhelp.com>
2022-06-16 07:15:12 -0700

    Format with Prettier

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,20 +1,35 @@
-import AuthRequired from '../helpers/AuthRequired'
-import Accordion from '../components/Accordion/'
-import Layout from '../components/Layout'
+import AuthRequired from "../helpers/AuthRequired";
+import Accordion from "../components/Accordion/";
+import Layout from "../components/Layout";
 
 export default function Dashboard() {
-
   return (
     <Layout>
-    <AuthRequired>
-      <div className="container" style={{ padding: '50px 0 100px 0' }}>
-        <Accordion key="1" title="Morning" iconUrl='something' tasks={[{complete: false, name: "mornin'"}, {complete: true, name: "Second morning thing"}]}></Accordion>
-        <Accordion key="2" title="Daytime" iconUrl='something' tasks={[{complete: false, name: "howdy'"}]}></Accordion>
-        <Accordion key="3" title="Evening" iconUrl='something' tasks={[{complete: false, name: "evenin'"}]}></Accordion>
-      </div>
-    </AuthRequired>
+      <AuthRequired>
+        <div className="container" style={{ padding: "50px 0 100px 0" }}>
+          <Accordion
+            key="1"
+            title="Morning"
+            iconUrl="something"
+            tasks={[
+              { complete: false, name: "mornin'" },
+              { complete: true, name: "Second morning thing" },
+            ]}
+          ></Accordion>
+          <Accordion
+            key="2"
+            title="Daytime"
+            iconUrl="something"
+            tasks={[{ complete: false, name: "howdy'" }]}
+          ></Accordion>
+          <Accordion
+            key="3"
+            title="Evening"
+            iconUrl="something"
+            tasks={[{ complete: false, name: "evenin'" }]}
+          ></Accordion>
+        </div>
+      </AuthRequired>
     </Layout>
-  )
-
-  
+  );
 }
