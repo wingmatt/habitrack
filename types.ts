ca7e78c7c6bbde4509bb6664a2420fbeb0d9fa62
wingmatt <matt@wingwebhelp.com>
2022-06-17 08:29:58 -0700

    Add Habit interface

--- types.ts
+++ types.ts
@@ -3,3 +3,16 @@ export interface TaskInterface {
   complete: boolean,
   accessibleBy?: string[]
 }
+
+export interface HabitInterface {
+  id: string,
+  created_at?: string,
+  owner: string,
+  accessibleBy: string[],
+  name: string,
+  description?: string,
+  complete: boolean,
+  lastCompleted: Date | null,
+  daysUntilRepeat: number
+  timeOfDay: "morning" | "daytime" | "evening"
+}
