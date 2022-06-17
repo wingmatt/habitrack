7f1c35d09953ec3bd476d928b541c7606f0362ec
wingmatt <matt@wingwebhelp.com>
2022-06-17 08:30:26 -0700

    Add helper to render correct Time of Day icon

---
+++ helpers/TimeOfDayIcon.tsx
@@ -0,0 +1,14 @@
+import { WiSunrise, WiDaySunny } from "react-icons/wi";
+import { BsMoonStars } from "react-icons/bs";
+
+export default function TimeOfDayIcon ({timeOfDay}) {
+  switch(timeOfDay) {
+    case 'morning':
+      return <WiSunrise/>
+    case 'daytime':
+      return <WiDaySunny/>
+    case 'evening':
+      return <BsMoonStars/>
+  }
+  
+}
