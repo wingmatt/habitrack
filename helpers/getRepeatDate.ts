b4bdd270f1d9b8cd033460158807535c01eae58b
wingmatt <matt@wingwebhelp.com>
2022-06-18 13:04:28 -0700

    Adjust calendar date string output

--- helpers/getRepeatDate.ts
+++ helpers/getRepeatDate.ts
@@ -11,9 +11,9 @@ export default function getRepeatDate(
   return dayjs(result).calendar(null, {
     sameDay: '[Today]', // The same day ( Today at 2:30 AM )
     nextDay: '[Tomorrow]', // The next day ( Tomorrow at 2:30 AM )
-    nextWeek: 'dddd', // The next week ( Sunday at 2:30 AM )
+    nextWeek: '[Next] dddd', // The next week ( Sunday at 2:30 AM )
     lastDay: '[Yesterday]', // The day before ( Yesterday at 2:30 AM )
     lastWeek: '[Last] dddd', // Last week ( Last Monday at 2:30 AM )
-    sameElse: 'DD/MM/YYYY' // Everything else ( 17/10/2011 )
+    sameElse: 'M/D/YYYY' // Everything else ( 17/10/2011 )
   })
 }
