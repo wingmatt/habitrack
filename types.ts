a07a6898d0334ebe709179228df63be62eb792b6
wingmatt <matt@wingwebhelp.com>
2022-06-22 22:37:59 -0700

    Prettier formatting

--- types.ts
+++ types.ts
@@ -1,18 +1,19 @@
 export interface TaskInterface {
-  name: string,
-  complete: boolean,
-  accessibleBy?: string[]
+  name: string;
+  complete: boolean;
+  accessibleBy?: string[];
 }
 
 export interface HabitInterface {
-  id: string,
-  created_at?: string,
-  owner: string,
-  accessibleBy: string[],
-  name: string,
-  description?: string,
-  complete: boolean,
-  lastCompleted: Date | null,
-  daysUntilRepeat: number
-  timeOfDay: "morning" | "daytime" | "evening"
+  id: string;
+  created_at?: string;
+  owner: string;
+  accessibleBy: string[];
+  name: string;
+  description?: string;
+  complete: boolean;
+  lastCompleted: Date | null;
+  daysUntilRepeat: number;
+  repeatDate?: Date | null;
+  timeOfDay: "morning" | "daytime" | "evening";
 }
