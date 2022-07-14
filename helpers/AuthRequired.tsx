40cea7c93c5c724cae6c7f609a1d6c081c121d72
wingmatt <matt@wingwebhelp.com>
2022-07-14 10:07:28 -0700

    Convert AuthRequired from .js to .tsx

--- helpers/AuthRequired.tsx
+++ helpers/AuthRequired.tsx
@@ -3,9 +3,11 @@ import { useRouter } from "next/router";
 import { AiOutlineLoading } from "react-icons/ai";
 import { supabase } from "../utils/supabaseClient";
 import { UserProvider } from "./UserProvider";
+import { Session } from "@supabase/supabase-js";
+import { Props } from "../types";
 
-export default function AuthRequired({ children }) {
-  const [session, setSession] = useState(undefined);
+export default function AuthRequired({ children }: Props) {
+  const [session, setSession] = useState<Session | null | undefined>(undefined);
   const router = useRouter();
   useEffect(() => {
     setSession(supabase.auth.session());
@@ -14,9 +16,10 @@ export default function AuthRequired({ children }) {
     });
   }, []);
   useEffect(() => {
-    if ((session === null) || (session?.user && session.user.email === undefined)) router.push("/login");
+    if (session === null || (session?.user && session.user.email === undefined))
+      router.push("/login");
   }, [session]);
-  if (session && session.user.email) {
+  if (session && session?.user?.email) {
     return <UserProvider>{children}</UserProvider>;
   } else {
     return <AiOutlineLoading />;
