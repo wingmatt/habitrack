341047b3f7bb4e2f0eb8c21bf53a312556086420
wingmatt <matt@wingwebhelp.com>
2022-07-16 09:44:18 -0700

    Add userEvents & failing test expectations

--- __tests__/HabitEditor.test.tsx
+++ __tests__/HabitEditor.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
 import HabitEditor from "../components/Habit/HabitEditor";
 import { useUserData } from "../helpers/UserProvider";
 
@@ -20,7 +21,9 @@ const mockState = {
 
 test("New Habit: Submitting a new habit should add that habit to the context", () => {
   //arrange
-  const {state, dispatch} = useUserData();
+  const user = userEvent.setup();
+    // TODO: Mock this with mockState
+  const state = mockState;
   render(
     <HabitEditor
       id=""
@@ -35,19 +38,25 @@ test("New Habit: Submitting a new habit should add that habit to the context", (
       timeOfDay="daytime"
     />
   );
+
+  const nameField = screen.getByLabelText('Name', {selector: 'input'})
+  const saveButton = screen.getByRole('button', {name: 'Save'});
+
   //act
-    // Fill out form with some basic data
-    // Click save
-    // Form should handle the dispatch
-    // Probably have to mock the supabase create call
+  user.type(nameField, 'Take out the trash');
+  user.click(saveButton);
 
   //assert
-    // Check that we have the ID from the mocked supabase call in the state, and the other stuff matches too
+  const newHabitInState = state.habits.find(habit => habit.name == "Take out the trash")
+  expect(newHabitInState).toBeTruthy;
+  expect(newHabitInState?.id).toBe('newIdFromSupabaseMock');
+  expect(state.habits.length).toBe(2);
 });
 
 test("Edit Habit: Submitting a habit with the ID of an existing habit should update the context", () => {
   //arrange
-  const {state, dispatch} = useUserData();
+  const user = userEvent.setup();
+  const state = mockState;
   render(
     <HabitEditor
       id="testId"
@@ -64,11 +73,14 @@ test("Edit Habit: Submitting a habit with the ID of an existing habit should upd
   );
   //act
 
-    // Change value of name and description
-    // Click "Save"
-    // The component should handle the dispatch
+  const nameField = screen.getByLabelText('Name', {selector: 'input'})
+  const saveButton = screen.getByRole('button', {name: 'Save'});
+  //act
+  user.type(nameField, 'New Name');
+  user.click(saveButton);
 
   //assert
   const habitInState = state.habits.find(habit => habit.id == "testId")
-  // Check that the state has the new info in it
+  expect(habitInState?.name).toBe('New Name');
+  expect(state.habits.length).toBe(1);
 });
