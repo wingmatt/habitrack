4e1c35093775b4c2770def3510cccd979c33fe2b
wingmatt <matt@wingwebhelp.com>
2022-07-16 18:11:27 -0700

    Add label to Loading icon. Prettier formatting

--- helpers/AuthRequired.tsx
+++ helpers/AuthRequired.tsx
@@ -5,17 +5,20 @@ import { useUserData } from "./UserProvider";
 import { Props } from "../types";
 
 export default function AuthRequired({ children }: Props) {
-  const {state} = useUserData();
+  const { state } = useUserData();
   const router = useRouter();
   const userSession = state?.user;
   useEffect(() => {
     const userSession = state?.user;
-    if (userSession === null || (userSession?.user && userSession.email === undefined))
+    if (
+      userSession === null ||
+      (userSession?.user && userSession.email === undefined)
+    )
       router.push("/login");
   }, []);
   if (userSession && userSession?.email) {
     return <>{children}</>;
   } else {
-    return <AiOutlineLoading />;
+    return <AiOutlineLoading aria-label="Loading" />;
   }
 }
