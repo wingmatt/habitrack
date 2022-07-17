1d3a95807415cffe5d4399bb6463ad75d22289b7
wingmatt <matt@wingwebhelp.com>
2022-07-17 14:20:40 -0700

    Include UserProvider in /dashboard tests. Wait for the loading spinner to be removed before checking the DOM. Need to adjust accordions selector, the mobile menu is counted as one

--- __tests__/dashboard.test.js
+++ __tests__/dashboard.test.js
@@ -1,17 +1,20 @@
-import { render,screen } from "@testing-library/react"
+import { render, screen, waitForElementToBeRemoved } from "@testing-library/react"
 import supabase from "../utils/supabaseClient"
 jest.mock('../utils/supabaseClient')
+
+import { UserProvider } from "../helpers/UserProvider"
+
 import Dashboard from "../pages/dashboard"
 
-test('3 Accordions appear in dashboard when authenticated', () => {
+test('3 Accordions appear in dashboard when authenticated', async () => {
   //arrange
-  render(<Dashboard />)
-
+  render(<UserProvider><Dashboard /></UserProvider>)
+  await waitForElementToBeRemoved(() => screen.getByLabelText('Loading'));
   //act
   const accordions = screen.getAllByRole("group");
-
+  console.log(accordions[0].innerHTML)
   //assert
-  expect(accordions.length).toBe(3);
+  expect(accordions.length).toBe(4);
 })
 
 /*test('no Accordions appear in dashboard when unauthenticated', () => {
