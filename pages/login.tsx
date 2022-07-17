da07022b551da95cef7e1c0ef217b44bf727dcd4
wingmatt <matt@wingwebhelp.com>
2022-07-16 18:15:35 -0700

    Use context, not state, for /login session data

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,27 +1,19 @@
-import { useState, useEffect } from "react";
-import { supabase } from "../utils/supabaseClient";
+import { useEffect } from "react";
 import Auth from "../components/Auth";
 import { AiOutlineLoading } from "react-icons/ai";
 import { useRouter } from "next/router";
-import { Session } from "@supabase/supabase-js";
+import { useUserData } from "../helpers/UserProvider";
+
 
 export default function Home() {
-  const [session, setSession] = useState<Session | null | undefined>(undefined);
+  const {state} = useUserData();
   const router = useRouter();
   useEffect(() => {
-    const supabaseSession = supabase.auth.session();
-    if (supabaseSession) setSession(supabaseSession);
-
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
+    if (!(state.user == null)) router.push("/dashboard");
   }, []);
-  useEffect(() => {
-    if (!(session === null)) router.push("/dashboard");
-  }, [session]);
   return (
     <div className="container" style={{ padding: "50px 0 100px 0" }}>
-      {!session ? <Auth /> : <AiOutlineLoading />}
+      {!state.user ? <Auth /> : <AiOutlineLoading />}
     </div>
   );
 }
