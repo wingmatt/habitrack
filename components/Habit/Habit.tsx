f72689e0e0f8880ef0325db18803c7c3b39b4ac1
wingmatt <matt@wingwebhelp.com>
2022-07-24 22:48:46 -0700

    Use Habit CSS module. Add edit habit Link

--- components/Habit/Habit.tsx
+++ components/Habit/Habit.tsx
@@ -1,18 +1,23 @@
 import TimeOfDayIcon from "../../helpers/TimeOfDayIcon"
 import getRepeatDate from "../../helpers/getRepeatDate";
+import Link from "next/link";
 import { FaEdit } from "react-icons/fa";
 import { HabitInterface } from "../../types";
+import styles from './Habit.module.css'
 
 
 
 export default function Habit(props: HabitInterface) {
   const repeatDate = getRepeatDate(props.lastCompleted, props.daysUntilRepeat)
   return (
-    <li>
+    <li class={styles.habit}>
       <TimeOfDayIcon timeOfDay={props.timeOfDay} />
       <span>{props.name}</span>
       <span>Repeats: {repeatDate}</span>
-      <FaEdit />
+      <Link href={'/habits/edit/' + props.id}>
+        <a><FaEdit /></a>
+      </Link>
+      
     </li>
   );
 }
