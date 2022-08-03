72e0537ec121715bd67c77469e7aec64c6641b90
wingmatt <matt@wingwebhelp.com>
2022-08-03 08:34:09 -0700

    Add Alert to Layout

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,13 +1,21 @@
 import Header from './Header'
 import styles from './Layout.module.css'
 import Link from "next/link";
+import { useUserData } from '../../helpers/UserProvider';
+import { Alert } from '@reach/alert';
+
+const clearAlert = (dispatch) => {
+  dispatch({type: "UPDATE_ALERT", payload: ""})
+}
 
 export default function Layout ({children}) {
+  const {state, dispatch} = useUserData();
 
   return (
     <>
       <Header/>
       <main className={styles.main}>
+      {state.alert ? <Alert type="polite" className={styles.alert}>{state.alert} <button onClick={() => clearAlert(dispatch)}>X</button></Alert> : ""}
         {children}
       </main>
       <footer className={styles.footer}>
