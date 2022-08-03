b6afe40fc090e6decd343de125dfe12412e52576
wingmatt <matt@wingwebhelp.com>
2022-08-03 08:35:43 -0700

    Prettier formatting

--- components/Habit/Habit.tsx
+++ components/Habit/Habit.tsx
@@ -1,23 +1,22 @@
-import TimeOfDayIcon from "../../helpers/TimeOfDayIcon"
+import TimeOfDayIcon from "../../helpers/TimeOfDayIcon";
 import getRepeatDate from "../../helpers/getRepeatDate";
 import Link from "next/link";
 import { FaEdit } from "react-icons/fa";
 import { HabitInterface } from "../../types";
-import styles from './Habit.module.css'
-
-
+import styles from "./Habit.module.css";
 
 export default function Habit(props: HabitInterface) {
-  const repeatDate = getRepeatDate(props.lastCompleted, props.daysUntilRepeat)
+  const repeatDate = getRepeatDate(props.lastCompleted, props.daysUntilRepeat);
   return (
-    <li class={styles.habit}>
+    <li className={styles.habit}>
       <TimeOfDayIcon timeOfDay={props.timeOfDay} />
       <span>{props.name}</span>
-      <span>Repeats: {repeatDate}</span>
-      <Link href={'/habits/edit/' + props.id}>
-        <a><FaEdit /></a>
+      <span>Repeats: {getRepeatDate(props.lastCompleted, props.daysUntilRepeat)}</span>
+      <Link href={"/habits/edit/" + props.id}>
+        <a>
+          <FaEdit />
+        </a>
       </Link>
-      
     </li>
   );
 }
