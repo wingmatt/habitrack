ab5231b871b06e94562f2e66802e75fa97dd7bf1
wingmatt <matt@wingwebhelp.com>
2022-08-03 08:49:29 -0700

    Replace Task placeholders with real habit data. Replace getTitleIcon with TimeOfDayIcon

--- components/Accordion/Accordion.tsx
+++ components/Accordion/Accordion.tsx
@@ -1,23 +1,15 @@
 import styles from "./Accordion.module.css"
 import { WiSunrise, WiDaySunny } from "react-icons/wi"
 import {BsMoonStars} from "react-icons/bs"
+import { FaEdit } from "react-icons/fa"
 import GemProgress from "./GemProgress"
 import Toggle from "./Toggle"
-import Task from "./Task"
 import { useState } from "react"
 import { TaskInterface } from '../../types'
-
-
-const getTitleIcon = (title: "Morning" | "Daytime" | "Evening") => {
-  switch (title) {
-    case "Morning":
-      return <WiSunrise />;
-    case "Daytime":
-      return <WiDaySunny />;
-    case "Evening":
-      return <BsMoonStars />;
-  }
-}
+import { useUserData } from "../../helpers/UserProvider"
+import TimeOfDayIcon from "../../helpers/TimeOfDayIcon"
+import getRepeatDate from "../../helpers/getRepeatDate"
+import Link from "next/link"
 
 const Accordion = ( props: {
   title: "Morning" | "Daytime" | "Evening",
@@ -25,18 +17,24 @@ const Accordion = ( props: {
   iconUrl: string,
 }) => {
   const [isOpen, setIsOpen] = useState (false);
+  const {state} = useUserData();
   return (
+    //@ts-ignore-next: Spread types may only be created from object types.ts(2698)
     <details {... isOpen ? "open" : ""} className={styles.details}>
       <summary onClick={() => setIsOpen(isOpen => !isOpen)} className={styles.summary} role="button">
         <div className={styles.titleContainer}>
-          {getTitleIcon(props.title)}
+          <TimeOfDayIcon timeOfDay={props.title} />
           <h2>{props.title}</h2>
         </div>
         <GemProgress tasksCompleted={0} />
         <Toggle isOpen={isOpen}/>
       </summary>
       <ul role="list">
-        {props.tasks.map(( task, index )=> <Task key={index} complete={task.complete} name={task.name} />)}
+        {state.habits.filter(habit => habit.timeOfDay == props.title.toLowerCase()).map((habit) => (
+          <li className={styles.habit}>
+            <span>{habit.name}</span>
+          </li>
+        ))}
       </ul>
     </details>
     )
--- helpers/TimeOfDayIcon.tsx
+++ helpers/TimeOfDayIcon.tsx
@@ -2,7 +2,7 @@ import { WiSunrise, WiDaySunny } from "react-icons/wi";
 import { BsMoonStars } from "react-icons/bs";
 
 export default function TimeOfDayIcon ({timeOfDay}) {
-  switch(timeOfDay) {
+  switch(timeOfDay.toLowerCase()) {
     case 'morning':
       return <WiSunrise/>
     case 'daytime':
