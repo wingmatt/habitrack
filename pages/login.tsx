f2dff5b6a1940c5cce3698b4c97f2fbb04c7cc9a
wingmatt <matt@wingwebhelp.com>
2022-08-18 08:25:41 -0700

    Get auth from context, not direct fetch. Add Layout to /profile, /login

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,8 +1,9 @@
 import { useEffect } from "react";
 import Auth from "../components/Auth";
-import { AiOutlineLoading } from "react-icons/ai";
 import { useRouter } from "next/router";
 import { useUserData } from "../helpers/UserProvider";
+import Layout from "../components/Layout";
+import Loading from "../components/Layout/Loading";
 
 
 export default function Home() {
@@ -12,8 +13,10 @@ export default function Home() {
     if (!(state.user == null)) router.push("/dashboard");
   }, []);
   return (
-    <div className="container" style={{ padding: "50px 0 100px 0" }}>
-      {!state.user ? <Auth /> : <AiOutlineLoading />}
-    </div>
+    <Layout>
+      <h1 className="header">Supabase + Next.js</h1>
+      <p className="description">Sign in via magic link with your email below</p>
+      {!state.user ? <Auth /> : <Loading />}
+    </Layout>
   );
 }
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -2,21 +2,15 @@ import { useState, useEffect } from 'react'
 import { supabase } from '../utils/supabaseClient'
 import Auth from '../components/Auth'
 import Account from '../components/Account'
+import Layout from '../components/Layout'
+import { useUserData } from '../helpers/UserProvider'
 
 export default function Home() {
-  const [session, setSession] = useState(null)
-
-  useEffect(() => {
-    setSession(supabase.auth.session())
-
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-  }, [])
+  const {state} = useUserData();
 
   return (
-    <div className="container" style={{ padding: '50px 0 100px 0' }}>
-      {!session ? <Auth /> : <Account key={session.user.id} session={session} />}
-    </div>
+    <Layout>
+      {!state.user ? <Auth /> : <Account key={state.user.id} session={state} />}
+    </Layout>
   )
 }
